29e1c0c6145f0caaa6a32d764d8f1ad6ece68e95
poojapandey305 <poojaam84@gmail.com>
2025-10-27 23:19:05 -0500

    Entity file Address.java added

---
+++ java-spring-boot-back-end-app/src/main/java/com/urbanspice/model/Address.java
@@ -0,0 +1,93 @@
+package com.urbanspice.model;
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "address")   // matches your ERD table name
+    public class Address {
+
+   //column name//
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "address_id")   //  primary key
+        private Long addressId;
+
+        @Column(name = "street")
+        private String street;
+
+        @Column(name = "city")
+        private String city;
+
+        @Column(name = "state")
+        private String state;
+
+        @Column(name = "zip_code")
+        private String zipCode;
+
+        // ---------- Constructors ----------
+        public Address() {}  // Default constructor 
+
+        public Address(String street, String city, String state, String zipCode) {
+            this.street = street;
+            this.city = city;
+            this.state = state;
+            this.zipCode = zipCode;
+        }
+
+        // ---------- Getters & Setters ----------
+        public Long getAddressId() {
+            return addressId;
+        }
+
+        public void setAddressId(Long addressId) {
+            this.addressId = addressId;
+        }
+
+        public String getStreet() {
+            return street;
+        }
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        public void setState(String state) {
+            this.state = state;
+        }
+
+        public String getZipCode() {
+            return zipCode;
+        }
+
+        public void setZipCode(String zipCode) {
+            this.zipCode = zipCode;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
