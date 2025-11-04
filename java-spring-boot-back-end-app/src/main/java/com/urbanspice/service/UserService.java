a3924f755eab0bc358d26d7e9423c0fce516dbaa
poojapandey305 <poojaam84@gmail.com>
2025-11-03 23:03:18 -0600

    UserService.java added

---
+++ java-spring-boot-back-end-app/src/main/java/com/urbanspice/service/UserService.java
@@ -0,0 +1,57 @@
+package com.urbanspice.service;
+import com.urbanspice.model.User;
+import com.urbanspice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    //crating a new user
+    public User crateUser(User user){
+        return userRepository.save(user);
+    }
+    //getting all the users
+    public List <User>getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    //getting user by id
+    public Optional<User>getUserById(Long id){
+        return userRepository.findById(id);
+    }
+
+    //Updating existing User
+    public User updateUser( Long id, User updatedUser){
+        Optional<User>existingOpt = userRepository.findById(id);
+        if(existingOpt.isPresent()) {
+            User existing = existingOpt.get();
+            existing.setName(updatedUser.getName());
+            existing.setEmail(updatedUser.getEmail());
+            return userRepository.save(existing);
+        }
+        return null;
+
+        }
+        //delete user by id
+    public void deleteUser(Long id){
+        userRepository.deleteById(id);
+    }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
