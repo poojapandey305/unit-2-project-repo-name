4a8ccf291d00f916f13d6b9c9a916f869cfb9a13
poojapandey305 <poojaam84@gmail.com>
2025-11-03 23:23:45 -0600

    AddressService.java added

---
+++ java-spring-boot-back-end-app/src/main/java/com/urbanspice/service/AddressService.java
@@ -0,0 +1,53 @@
+package com.urbanspice.service;
+
+
+
+import com.urbanspice.model.Address;
+import com.urbanspice.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AddressService {
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    // Create new address
+    public Address createAddress(Address address) {
+        return addressRepository.save(address);
+    }
+
+    // Get all addresses
+    public List<Address> getAllAddresses() {
+        return addressRepository.findAll();
+    }
+
+    //  Get address by ID
+    public Optional<Address> getAddressById(Long id) {
+        return addressRepository.findById(id);
+    }
+
+    // Update existing address
+    public Address updateAddress(Long id, Address updatedAddress) {
+        Optional<Address> existingOpt = addressRepository.findById(id);
+        if (existingOpt.isPresent()) {
+            Address existing = existingOpt.get();
+            existing.setStreet(updatedAddress.getStreet());
+            existing.setCity(updatedAddress.getCity());
+            existing.setState(updatedAddress.getState());
+            existing.setZipCode(updatedAddress.getZipCode());
+
+            return addressRepository.save(existing);
+        }
+        return null;
+    }
+
+    // Delete address
+    public void deleteAddress(Long id) {
+        addressRepository.deleteById(id);
+    }
+}
