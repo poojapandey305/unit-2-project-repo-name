0be3f27a2341f124f1f594ea9df666f80d57f74a
poojapandey305 <poojaam84@gmail.com>
2025-11-08 23:58:26 -0600

    Added Cart entity for Cart module

---
+++ java-spring-boot-back-end-app/src/main/java/com/urbanspice/model/Cart.java
@@ -0,0 +1,37 @@
+package com.urbanspice.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+
+    // Many carts can belong to one user
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // One cart can contain many items
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> cartItems;
+
+    public Cart() {}
+
+    public Cart(User user) {
+        this.user = user;
+    }
+
+    // Getters & Setters
+    public Long getCartId() { return cartId; }
+    public void setCartId(Long cartId) { this.cartId = cartId; }
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public List<CartItem> getCartItems() { return cartItems; }
+    public void setCartItems(List<CartItem> cartItems) { this.cartItems = cartItems; }
+}
