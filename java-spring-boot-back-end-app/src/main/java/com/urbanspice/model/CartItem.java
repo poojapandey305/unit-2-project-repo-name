5d6c6036a472a18117d7e01810058199360bfeb9
poojapandey305 <poojaam84@gmail.com>
2025-11-09 15:17:24 -0600

    added CartItem.java with Intity relationship

--- java-spring-boot-back-end-app/src/main/java/com/urbanspice/model/CartItem.java
+++ java-spring-boot-back-end-app/src/main/java/com/urbanspice/model/CartItem.java
@@ -9,15 +9,25 @@ public class CartItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name= "cart_item_id")
     private Long cartItemId;
-private Long cartId;
-private Long itemId;
-private int quantity;
+
+    //entity relationship
+    // many items can belong to one cart
+    @ManyToOne
+    @JoinColumn(name= "cart_id")
+    private Cart cart;
+    //many cart item can refer to one menuItem
+    @ManyToOne
+    @JoinColumn(name="item_id")
+    private MenuItem menuItem;
+
+
+   private int quantity;
 
     public CartItem() {}
 
-    public CartItem(Long cartId,Long itemId, int quantity) {
-        this.cartId = cartId;
-        this.itemId =itemId ;
+    public CartItem(Cart cart,MenuItem menuItem, int quantity) {
+        this.cart = cart;
+        this.menuItem = menuItem;
         this.quantity = quantity;
     }
 
@@ -25,11 +35,11 @@ private int quantity;
     public Long getCartItemId() { return cartItemId; }
     public void setCartItemId(Long cartItemId) { this.cartItemId = cartItemId; }
 
-    public Long getCartId() { return cartId; }
-    public void setCartId(Long cartId) { this.cartId = cartId; }
+    public Cart getCart() { return cart; }
+    public void setCart(Cart cart) { this.cart = cart; }
 
-    public Long getItemId() { return itemId; }
-    public void setMenuItem(Long menuItem) { this.itemId = itemId; }
+    public MenuItem getMenuItem() { return menuItem; }
+    public void setMenuItem(MenuItem menuItem) { this.menuItem = menuItem; }
 
     public int getQuantity() { return quantity; }
     public void setQuantity(int quantity) { this.quantity = quantity; }
