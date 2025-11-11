d0d571b04b07832ef0649d125297aa1fb6954698
poojapandey305 <poojaam84@gmail.com>
2025-11-11 13:28:34 -0600

    Added PaymentService class with CRUD methods

---
+++ java-spring-boot-back-end-app/src/main/java/com/urbanspice/service/PaymentService.java
@@ -0,0 +1,32 @@
+
+package com.urbanspice.service;
+
+import com.urbanspice.model.Payment;
+import com.urbanspice.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PaymentService {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    public Payment savePayment(Payment payment) {
+        return paymentRepository.save(payment);
+    }
+
+    public List<Payment> getAllPayments() {
+        return paymentRepository.findAll();
+    }
+
+    public Payment getPaymentById(Long paymentId) {
+        return paymentRepository.findById(paymentId).orElse(null);
+    }
+
+    public void deletePayment(Long paymentId) {
+        paymentRepository.deleteById(paymentId);
+    }
+}
