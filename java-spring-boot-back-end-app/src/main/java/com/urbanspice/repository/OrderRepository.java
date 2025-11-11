03d1d135d7fc1d14b3a74f829ab693c347c1bc5e
poojapandey305 <poojaam84@gmail.com>
2025-11-11 11:43:34 -0600

    added orderRepository

---
+++ java-spring-boot-back-end-app/src/main/java/com/urbanspice/repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.urbanspice.repository;
+
+import com.urbanspice.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+     List<Order> findByUser_UserId(Long userId);
+}
