28dbdd7200a92f3694ff99b2a5b9bb0c9e331c08
poojapandey305 <poojaam84@gmail.com>
2025-11-11 12:16:08 -0600

    added ordercontroller

---
+++ java-spring-boot-back-end-app/src/main/java/com/urbanspice/controller/OrderController.java
@@ -0,0 +1,37 @@
+package com.urbanspice.controller;
+
+import com.urbanspice.model.Order;
+import com.urbanspice.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+@CrossOrigin(origins = "http://localhost:5174")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.saveOrder(order);
+    }
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("/{orderId}")
+    public Order getOrderById(@PathVariable Long orderId) {
+        return orderService.getOrderById(orderId);
+    }
+
+    @DeleteMapping("/{orderId}")
+    public void deleteOrder(@PathVariable Long orderId) {
+        orderService.deleteOrder(orderId);
+    }
+}
