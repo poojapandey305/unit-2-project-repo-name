bb98fa2ea3eec67ddb6a1465afb8bf53b477f3ab
poojapandey305 <poojaam84@gmail.com>
2025-11-11 13:29:49 -0600

    Added PaymentController with REST endpoints

---
+++ java-spring-boot-back-end-app/src/main/java/com/urbanspice/controller/PaymentController.java
@@ -0,0 +1,37 @@
+package com.urbanspice.controller;
+
+import com.urbanspice.model.Payment;
+import com.urbanspice.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/payments")
+@CrossOrigin(origins = "http://localhost:5174")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping
+    public Payment createPayment(@RequestBody Payment payment) {
+        return paymentService.savePayment(payment);
+    }
+
+    @GetMapping
+    public List<Payment> getAllPayments() {
+        return paymentService.getAllPayments();
+    }
+
+    @GetMapping("/{paymentId}")
+    public Payment getPaymentById(@PathVariable Long paymentId) {
+        return paymentService.getPaymentById(paymentId);
+    }
+
+    @DeleteMapping("/{paymentId}")
+    public void deletePayment(@PathVariable Long paymentId) {
+        paymentService.deletePayment(paymentId);
+    }
+}
